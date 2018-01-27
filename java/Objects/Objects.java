dc2c2bc4d3abc0ca169a4fc6ccb3d1a5c0a1d514
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2018-01-27 16:13:22 +0100

    Simplified object references

--- java/Objects/Objects.java
+++ java/Objects/Objects.java
@@ -1,7 +1,6 @@
 public class Objects {
     public static void main(String[] args) {
-    	StringBuilder sb = new StringBuilder("one");
-    	sb.append(", two");
-    	String result = sb.toString();
+    	Object o = new Object();
+    	o.toString();
     }
 }
