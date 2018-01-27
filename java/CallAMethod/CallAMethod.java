636440d0b076309119f35c313a448e0f8e9ffe33
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2018-01-27 16:13:14 +0100

    Simplified method calling

--- java/CallAMethod/CallAMethod.java
+++ java/CallAMethod/CallAMethod.java
@@ -2,10 +2,10 @@ public class CallAMethod {
     public static void main(String[] args) {
     	int a = 42; // 0x2A in hex
     	int b = 4;
-    	int result = add(a, b, 0);
+    	int result = add(a, b);
     }
 
-    private static int add(int a, int b, int c) {
+    private static int add(int a, int b) {
     	return a + b;
     }
 }
